09b41db9693a95b0f5ee2385c2b5b550ba9d05bc
Patrick Gross <bears_lair@protonmail.com>
2023-12-04 13:58:01 -0800

    game() work in progress

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 function getComputerChoice() {
-    //Switch statement for Rock, Paper, Scissors based on randomly chosen number
     let computerSelection = "";
+    //Random number from 0-2 chosen
+    let computerChoice = Math.floor(Math.random() * 3);
 
-    switch (Math.floor(Math.random() * 3)) {
-        case 0:
-            computerSelection = "rock";
-            break;
-        case 1:
-            computerSelection = "paper";
-            break;
-        case 2:
-            computerSelection = "scissors";
-            break;
+    //If-else statement to translate number to string
+    if (computerChoice = 0) {
+        return computerSelection = "rock";
+    } else if (computerChoice = 1) {
+        return computerSelection = "paper";
+    } else if (computerChoice = 2) {
+        return computerSelection = "scissors";
     }
-    
-    //Return that variable out of the function
-    return computerSelection;
 };
 
 function playRound(playerSelection,computerSelection) {
@@ -23,29 +18,31 @@ function playRound(playerSelection,computerSelection) {
     let roundResult = "";
     //If-else statements for all possible cases, including ties
     if (playerSelection == "rock" && computerSelection == "rock") {
-        roundResult = "Tied!";
+        return roundResult = "Tied!";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        roundResult = "You lose! Paper beats rock."
+        return roundResult = "You lose! Paper beats rock."
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        roundResult = "You win! Rock beats scissors.";
+        return roundResult = "You win! Rock beats scissors.";
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        roundResult = "You win! Paper beats rock.";
+        return roundResult = "You win! Paper beats rock.";
     } else if (playerSelection == "paper" && computerSelection == "paper") {
-        roundResult = "Tied!";
+        return roundResult = "Tied!";
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        roundResult = "You lose! Scissors beats paper.";
+        return roundResult = "You lose! Scissors beats paper.";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        roundResult = "You lose! Rock beats scissors.";
+        return roundResult = "You lose! Rock beats scissors.";
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        roundResult = "Tied";
+        return roundResult = "Tied";
     }
-
-    return roundResult;
 };
 
 function game() {
-    //For loop 5 times
-        //Prompt the user for their selection of either rock, paper, or scissors (not case-senstive), assign to playerSelection variable
-        //Call getComputerChoice
-        //Evaluate result with playRound, end of current loop
+    //Prompt the user for their selection of either rock, paper, or scissors (not case-senstive), assign to playerSelection variable
+    let playerSelection = prompt("Rock, paper, or scissors?");
+    //Call getComputerChoice
+    getComputerChoice();
+    //Evaluate winner
+    playRound(playerSelection,computerSelection)
+    //Print winner to screen
+    alert(roundResult);
 }
